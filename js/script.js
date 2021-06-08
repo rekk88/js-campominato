b6c20b45c795bb4a9419ce7499b1822009543595
maicol <maicol.matteoli@gmail.com>
2021-06-08 15:20:31 +0200

    add : input numeri utente senza doppioni

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 
 document.getElementById("start").addEventListener("click",function(){
     var random;
-    var numbers = [];
+    var numbers = []; //array dei numeri generati dal PC
+    var vet = []; //array dei numeri inseriti dall'utente
+    var x; //variabile per l'input dell'utente
+
+    //genero i 16 numeri random
     for(var i =0; i < 16 ; i++){
         random = rand();
         if(!numbers.includes(random)){
@@ -11,7 +15,20 @@ document.getElementById("start").addEventListener("click",function(){
             i--;
         }
     }
-    console.log(numbers);
+   
+    for(var i=0 ; i < 5; i++){
+        //input di un max di 84 numeri senza ripetizioni 
+        x = parseInt(prompt("inserisci un numero : "));
+        if(!vet.includes(x)){ //se il numero inserito non è già stato inserito
+            vet.push(x);
+        } 
+        else{ //il numero è già stato inserito
+            i--;
+            alert("NOH !!!! Il numero inserito è già presente");
+        }
+  
+    }
+    // console.log(numbers);
     // console.log(random);
 });
 
